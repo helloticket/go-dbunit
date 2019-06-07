34ff4472553c5f467cc41a84d9d7d03fd824f57a
Helder Guilherme Farias <helder@Helders-MacBook-Pro.local>
2019-06-07 17:52:05 -0300

    Tratar erros sem matar suite

--- postgres.go
+++ postgres.go
@@ -14,7 +14,7 @@ type PostgresDatabaseFactory struct {
 func (p *PostgresDatabaseFactory) Exec(cmds []Command) {
 	for _, c := range cmds {
 		if _, err := p.db.NamedExec(c.sql, c.record.Values()); err != nil {
-			log.Fatal("file:", c.record.fileName, " error:", err, " sql:", c.sql, " values:", c.record.values)
+			log.Println("file:", c.record.fileName, " error:", err, " sql:", c.sql, " values:", c.record.values)
 		}
 	}
 }
